4c458220a48d68804397a578cf098889c32cf77b
MtDeity <o8olo853q43@gmail.com>
2020-03-04 13:37:17 +0900

    storyNumber into a private property

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -39,40 +39,40 @@ class StoryBrain {
         choice2: '')
   ];
 
-  int storyNumber = 0;
+  int _storyNumber = 0;
 
   String getStory() {
-    return _storyData[storyNumber].storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
   String getChoice1() {
-    return _storyData[storyNumber].choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
   String getChoice2() {
-    return _storyData[storyNumber].choice2;
+    return _storyData[_storyNumber].choice2;
   }
 
   void nextStory(int userChoice) {
-    if (storyNumber == 0 && userChoice == 1) {
-      storyNumber = 2;
-    } else if (storyNumber == 0 && userChoice == 2) {
-      storyNumber = 1;
-    } else if (storyNumber == 1 && userChoice == 1) {
-      storyNumber = 2;
-    } else if (storyNumber == 1 && userChoice == 2) {
-      storyNumber = 3;
-    } else if (storyNumber == 2 && userChoice == 1) {
-      storyNumber = 5;
-    } else if (storyNumber == 2 && userChoice == 2) {
-      storyNumber = 4;
-    } else if (storyNumber == 3 || storyNumber == 4 || storyNumber == 5) {
+    if (_storyNumber == 0 && userChoice == 1) {
+      _storyNumber = 2;
+    } else if (_storyNumber == 0 && userChoice == 2) {
+      _storyNumber = 1;
+    } else if (_storyNumber == 1 && userChoice == 1) {
+      _storyNumber = 2;
+    } else if (_storyNumber == 1 && userChoice == 2) {
+      _storyNumber = 3;
+    } else if (_storyNumber == 2 && userChoice == 1) {
+      _storyNumber = 5;
+    } else if (_storyNumber == 2 && userChoice == 2) {
+      _storyNumber = 4;
+    } else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
       restart();
     }
   }
 
   void restart() {
-    storyNumber = 0;
+    _storyNumber = 0;
   }
 }
 
